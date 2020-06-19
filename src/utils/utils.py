ccfac845c163d269ab3b1c69fb13459578f8acaa
jimzers <adamleesaintfrancis@gmail.com>
2020-06-18 22:04:49 -0700

    fixed utils py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,15 +4,17 @@ import torch.nn as nn
 import matplotlib
 import matplotlib.pyplot as plt
 
-def make_mlp(input_dim, hidden_dims, output_dim, activation_fn, output_activation_fn=nn.Identity):
-    layer_arr = [input_dim] + list(hidden_dims) + [output_dim]
+def make_mlp(layer_arr, activation_fn, output_activation_fn=nn.Identity):
+    # layer_arr = [input_dim] + list(hidden_dims) + [output_dim]
     model = []
 
     for i in range(len(layer_arr) - 1):
+        # print(layer_arr)
+        # print(layer_arr[i])
         model += [nn.Linear(layer_arr[i], layer_arr[i+1])]
-        if i != len(layer_arr) - 1:
+        if i != len(layer_arr) - 2:
             model += [activation_fn()]
 
-        model += [output_activation_fn()]
+    model += [output_activation_fn()]
 
     return nn.Sequential(*model)
